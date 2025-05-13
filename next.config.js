87cb8afa5365de15398411a950bde0b264d42fbc
Lennart Vincent Bart <lennart7007@web.de>
2025-05-13 10:06:32 +0200

    chore: remove .eslintrc.json and update next.config.js to simplify configuration; update package dependencies and devDependencies for improved compatibility

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '*.s3.eu-central-1.amazonaws.com', // todo whitelist bucket explicitly
-          port: '',
-          pathname: '/projects/**',
-          //search: '',
-        },
-      ],
-    },
-  }
+const nextConfig = {}
 
 module.exports = nextConfig
