dbb71e9716981a6d4c45bb3731f65951a044e2aa
Lennart Vincent Bart <lennart7007@web.de>
2025-09-21 23:34:00 +0200

    chore: refine configuration files for Next.js and Tailwind CSS, improving structure and clarity

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
-// Next.js configuration
-// - Allow optimized images from project S3 bucket
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '*.s3.eu-central-1.amazonaws.com',
-          port: '',
-          pathname: '/projects/**',
-          
-        },
-      ],
-    },
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.s3.eu-central-1.amazonaws.com', // todo whitelist bucket explicitly
+        port: '',
+        pathname: '/projects/**',
+        //search: '',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
